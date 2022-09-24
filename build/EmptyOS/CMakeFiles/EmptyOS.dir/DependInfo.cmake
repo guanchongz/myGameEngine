919c08beaa6906a058e5285c88df1e47d9c525b0
管冲 <502187489@qq.com>
2022-09-24 22:50:15 +0800

    create FrameWork
    创建一个面向多平台的引擎框架

---
+++ build/EmptyOS/CMakeFiles/EmptyOS.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/guan/myGameEngine/EmptyOS/EmptyApplication.cpp" "EmptyOS/CMakeFiles/EmptyOS.dir/EmptyApplication.cpp.o" "gcc" "EmptyOS/CMakeFiles/EmptyOS.dir/EmptyApplication.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/guan/myGameEngine/build/Framework/Common/CMakeFiles/Common.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/EmptyOS/CMakeFiles/EmptyOS.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EmptyOS.dir/EmptyApplication.cpp.o"
+  "CMakeFiles/EmptyOS.dir/EmptyApplication.cpp.o.d"
+  "EmptyOS"
+  "EmptyOS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EmptyOS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
